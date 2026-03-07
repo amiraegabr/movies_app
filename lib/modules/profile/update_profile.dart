4f6d0afc2a682fc7dde1283e7a6ead77966e98eb
Amira Gabr <amira.ehab.gabr@gmail.com>
2026-03-07 06:31:38 +0200

    firebase authentication; Login, Register, Forgot Password, Update profile

--- lib/modules/profile/update_profile.dart
+++ lib/modules/profile/update_profile.dart
@@ -1,8 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:movies_app/modules/widgets/custom_textformfield.dart';
 
 import '../../core/gen/assets.gen.dart';
+import '../../core/routes/page_route_names.dart';
 import '../../core/theme/app_colors.dart';
+import '../../models/avatars_list.dart';
 
 class UpdateProfile extends StatefulWidget {
   const UpdateProfile({super.key});
@@ -13,19 +17,56 @@ class UpdateProfile extends StatefulWidget {
 
 class _UpdateProfileState extends State<UpdateProfile> {
   AssetGenImage selectedAvatar = Assets.images.avatars.avatar1;
+  final List<AssetGenImage> avatars = Avatars;
 
-  // List of all avatars
-  final List<AssetGenImage> avatars = [
-    Assets.images.avatars.avatar1,
-    Assets.images.avatars.avatar2,
-    Assets.images.avatars.avatar3,
-    Assets.images.avatars.avatar4,
-    Assets.images.avatars.avatar5,
-    Assets.images.avatars.avatar6,
-    Assets.images.avatars.avatar7,
-    Assets.images.avatars.avatar8,
-    Assets.images.avatars.avatar9,
-  ];
+  final nameController = TextEditingController();
+  final phoneController = TextEditingController();
+
+  int avatarIndex = 0;
+  bool isLoading = true;
+
+  Future<void> loadUserData() async {
+    final uid = FirebaseAuth.instance.currentUser!.uid;
+
+    final doc = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(uid)
+        .get();
+
+    final data = doc.data();
+
+    if (data != null) {
+      nameController.text = data['name'] ?? '';
+      phoneController.text = data['phone'] ?? '';
+      avatarIndex = data['avatarIndex'] ?? 0;
+
+      selectedAvatar = avatars[avatarIndex];
+    }
+
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  Future<void> updateProfile() async {
+    final uid = FirebaseAuth.instance.currentUser!.uid;
+
+    await FirebaseFirestore.instance.collection('users').doc(uid).update({
+      "name": nameController.text.trim(),
+      "phone": phoneController.text.trim(),
+      "avatarIndex": avatarIndex,
+    });
+
+    ScaffoldMessenger.of(
+      context,
+    ).showSnackBar(const SnackBar(content: Text("Profile updated")));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadUserData();
+  }
 
   void _showAvatarPicker() {
     showModalBottomSheet(
@@ -57,7 +98,10 @@ class _UpdateProfileState extends State<UpdateProfile> {
 
                   return GestureDetector(
                     onTap: () {
-                      setState(() => selectedAvatar = avatar);
+                      setState(() {
+                        selectedAvatar = avatar;
+                        avatarIndex = index;
+                      });
                       Navigator.pop(context);
                     },
                     child: Container(
@@ -93,55 +137,71 @@ class _UpdateProfileState extends State<UpdateProfile> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Pick Avatar')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          spacing: 20,
-          children: [
-            GestureDetector(
-              onTap: _showAvatarPicker,
-              child: Center(
-                child: SizedBox(
-                  width: 150,
-                  height: 150,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(75),
-                    child: selectedAvatar.image(fit: BoxFit.cover),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            spacing: 20,
+            children: [
+              GestureDetector(
+                onTap: _showAvatarPicker,
+                child: Center(
+                  child: SizedBox(
+                    width: 150,
+                    height: 150,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(75),
+                      child: selectedAvatar.image(fit: BoxFit.cover),
+                    ),
                   ),
                 ),
               ),
-            ),
-            CustomTextFormField(
-              hint: 'User Name',
-              prefixIcon: Padding(
-                padding: EdgeInsets.all(8),
-                child: Assets.icons.idIcon.svg(width: 24),
+              CustomTextFormField(
+                controller: nameController,
+                hint: nameController.text,
+                prefixIcon: Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Assets.icons.idIcon.svg(width: 24),
+                ),
               ),
-            ),
-            CustomTextFormField(
-              hint: '01000000000',
-              prefixIcon: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Assets.icons.phoneIcon.svg(width: 24),
+              CustomTextFormField(
+                controller: phoneController,
+                hint: phoneController.text,
+                prefixIcon: Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Assets.icons.phoneIcon.svg(width: 24),
+                ),
               ),
-            ),
-            Text(
-              'Reset Password',
-              style: TextTheme.of(context).bodyLarge,
-              textAlign: TextAlign.end,
-            ),
-            Spacer(),
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                backgroundColor: AppColors.red,
-                foregroundColor: AppColors.white,
+              Align(
+                alignment: Alignment.centerRight,
+                child: TextButton(
+                  onPressed: () => Navigator.pushNamed(
+                    context,
+                    PageRouteNames.forgotPassword,
+                  ),
+                  child: Text(
+                    'Reset Password',
+                    style: TextTheme.of(context).bodyLarge,
+                    textAlign: TextAlign.end,
+                  ),
+                ),
               ),
-              child: Text('Delete Account'),
-            ),
-            ElevatedButton(onPressed: () {}, child: Text('Update Data')),
-          ],
+              Spacer(),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: AppColors.red,
+                  foregroundColor: AppColors.white,
+                ),
+                child: Text('Delete Account'),
+              ),
+              ElevatedButton(
+                onPressed: updateProfile,
+                child: Text('Update Data'),
+              ),
+            ],
+          ),
         ),
       ),
     );
